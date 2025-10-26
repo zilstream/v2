15194f2f73f0c99afafe2aa6b3506cb0ea841670
Melvin Beemer <melvinbeemer@me.com>
2025-10-26 16:48:15 +0700

    Add CF caching

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,18 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import {
+  withFilter,
+  softTagFilter,
+} from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  incrementalCache: r2IncrementalCache,
+  queue: doQueue,
+  tagCache: withFilter({
+    tagCache: d1NextTagCache,
+    filterFn: softTagFilter,
+  }),
+  enableCacheInterception: true,
+});
