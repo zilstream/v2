4cc9c4049ec014c70d8d67470865a1d6d2b67ab3
Melvin Beemer <melvinbeemer@me.com>
2025-11-14 17:38:37 +0700

    Add redirect for token detail links from wallets

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/token/:address",
+        destination: "/tokens/:address",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
