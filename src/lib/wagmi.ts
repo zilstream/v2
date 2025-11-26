cfd5a91dabd9a2b9124590f3b05c3bbeee55c20f
Melvin Beemer <melvinbeemer@me.com>
2025-11-26 11:05:47 +0700

    Specify wallet connectors

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,31 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  metaMaskWallet,
+  rabbyWallet,
+  injectedWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { createConfig, http } from "wagmi";
 import { zilliqa } from "viem/chains";
 
-export const config = getDefaultConfig({
-  appName: "ZilStream",
-  projectId: "9a73085bd2b8459dae2e269e6849eee1",
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [metaMaskWallet, rabbyWallet, injectedWallet, walletConnectWallet],
+    },
+  ],
+  {
+    appName: "ZilStream",
+    projectId: "9a73085bd2b8459dae2e269e6849eee1",
+  }
+);
+
+export const config = createConfig({
+  connectors,
   chains: [zilliqa],
   ssr: true,
+  transports: {
+    [zilliqa.id]: http(),
+  },
 });
